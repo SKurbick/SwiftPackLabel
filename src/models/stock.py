3ea47e7d72a5e4434e6d03a059f55f49224f1a3e
uventus <uventus_work@mail.ru>
2025-08-14 14:37:12 +0300

    Реализована сортировка заказов по полю createdAt в обратном порядке (от   новых к старым) внутри каждой группы wild-кодов для корректного   отображения хронологии заказов

--- src/models/stock.py
+++ src/models/stock.py
@@ -37,7 +37,7 @@ class StockDB:
             Если количество не является целым числом, возвращается 0.
         """
 
-        query = """SELECT product_id ,available_quantity from current_balances WHERE product_id = ANY($1)"""
+        query = """SELECT product_id ,available_quantity from current_balances WHERE product_id = ANY($1) AND warehouse_id = 1"""
         result = await self.db.fetch(query, wilds)
         return {res['product_id']: int(res['available_quantity'])
                 for res in result}
