c7fdaa32ea033a9d55eb303a2459022ef4b0617b
etojelev <lion.patskevich@yandex.ru>
2026-01-30 10:47:47 +0300

    Fix: Disabling reconnection attempts when receiving a 404 error

--- src/response.py
+++ src/response.py
@@ -36,6 +36,10 @@ class HttpClient:
                 response.raise_for_status()
                 return response.text
             except requests.RequestException as e:
+                status_code = e.response.status_code if e.response else None
+                if status_code == '404':
+                    logger.warning(f'Получен статус код 404 для метода {method} {url}. Повторные попытки отменены.')
+                    return None
                 logger.warning(f"Попытка {attempt + 1}: Ошибка во время {method} {url} - {e}")
                 time.sleep(self.delay)
         return None
