79e8441c0b9df006e7cfe913ff21c7e3be6ee194
Лев <levpat@MacBook-Pro-Lev.local>
2026-07-09 12:17:41 +0300

    Fix: fixing WB API query for excluding supplies from FBS 2.0

--- src/wildberries_api/supplies.py
+++ src/wildberries_api/supplies.py
@@ -24,7 +24,7 @@ class Supplies(Account):
             datas_for_extend = []
             for sup in data.get("supplies"):
                 sup_name: str = sup.get("name")
-                if not sup_name.startswith("supply for ["):
+                if not sup_name.startswith("supply for [") or not sup_name.startswith("ФИКТИВ_"):
                     datas_for_extend.append(sup)
             supplies.extend(datas_for_extend)
             next_value = data.get("next")
